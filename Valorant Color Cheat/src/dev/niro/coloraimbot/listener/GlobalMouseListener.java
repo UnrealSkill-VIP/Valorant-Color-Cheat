48784c41dc684d508d20784e580b536bfd435fa1
NiroDev <nicof1207@gmail.com>
2020-04-24 16:12:45 +0200

    .

---
+++ Valorant Color Cheat/src/dev/niro/coloraimbot/listener/GlobalMouseListener.java
@@ -0,0 +1,57 @@
+package dev.niro.coloraimbot.listener;
+
+import org.jnativehook.mouse.NativeMouseEvent;
+import org.jnativehook.mouse.NativeMouseInputListener;
+
+import dev.niro.coloraimbot.Main;
+
+public class GlobalMouseListener implements NativeMouseInputListener {
+	
+	@Override
+	public void nativeMouseClicked(NativeMouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void nativeMousePressed(NativeMouseEvent e) {
+		if(e.getButton() == 2 || e.getButton() == 5)
+			Main.working = true;
+		
+		if(e.getButton() == 2)
+			Main.inVisirSince = System.currentTimeMillis();
+		
+		if(e.getButton() == 1) {
+			Main.lastShot = System.currentTimeMillis();
+			Main.inFire = true;
+			Main.inVisirSince = System.currentTimeMillis() + Main.reviseTime;
+		}
+	}
+
+	@Override
+	public void nativeMouseReleased(NativeMouseEvent e) {
+		if(e.getButton() == 2 || e.getButton() == 5)
+			Main.working = false;
+		
+		if(e.getButton() == 2)
+			Main.inVisirSince = -1;
+		
+		if(e.getButton() == 1) {
+			Main.lastShot = System.currentTimeMillis();
+			Main.inFire = false;
+		}
+	}
+
+	@Override
+	public void nativeMouseDragged(NativeMouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void nativeMouseMoved(NativeMouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ Valorant Color Cheat/src/dev/niro/coloraimbot/utils/Logger.java
@@ -0,0 +1,23 @@
+package dev.niro.coloraimbot.utils;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Logger {
+
+	public static void log(Object text) {
+		System.out.println(getCurrentTimeStamp() + text);
+	}
+	
+	public static void err(String text) {
+		System.err.println(getCurrentTimeStamp() + text);
+	}
+	
+	public static String getCurrentTimeStamp() {
+	    SimpleDateFormat sdfDate = new SimpleDateFormat("[yyyy-MM-dd_HH:mm:ss] ");
+	    Date now = new Date();
+	    String strDate = sdfDate.format(now);
+	    return strDate;
+	}
+	
+}
